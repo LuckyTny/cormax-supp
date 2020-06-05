4263865bde7f03381e58a6893acf6ac89dbd5b78
Athony A <astremsky@gmail.com>
2020-06-04 23:53:11 -0400

    display: flex;

---
+++ src/components/ActiveQuiz/ActiveQuiz.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './ActiveQuiz.module.css'
+
+const ActiveQuiz = props => (
+    <div className={styles.ActiveQuiz}>
+        ActiveQuiz component works!
+    </div>
+)
+
+export default ActiveQuiz
