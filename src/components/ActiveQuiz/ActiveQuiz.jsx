e6be6411601a1e9b836998e09a1392df4eee2bb2
Athony A <astremsky@gmail.com>
2020-06-05 23:27:09 -0400

    Questions layout created

--- src/components/ActiveQuiz/ActiveQuiz.jsx
+++ src/components/ActiveQuiz/ActiveQuiz.jsx
@@ -3,7 +3,20 @@ import styles from './ActiveQuiz.module.css'
 
 const ActiveQuiz = props => (
     <div className={styles.ActiveQuiz}>
-        ActiveQuiz component works!
+        <p className = {styles.Question}>
+            <span>
+            <strong>1.</strong>
+            ActiveQuiz component works!
+            </span>
+            <small>4 from 12</small>
+        </p>
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+        </ul>
+        
     </div>
 )
 
--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -1,10 +1,18 @@
 import React, {Component} from 'react'
 import styles from './Quiz.module.css'
+import ActiveQuiz from '../../components/ActiveQuiz/ActiveQuiz'
+
 class Quiz extends Component {
+
+    state = {
+        quiz: []
+    }
+
     render() {
         return (
         <div className={styles.Quiz}>
             <h1> Quiz Container</h1>
+            <ActiveQuiz></ActiveQuiz>
         </div>
         )
     }
